8f0581bdadf6428f1cc634e3eb6abfdb91a8104f
LJ <imnotlostrui@aim.com>
2017-07-23 18:08:43 -0400

    added V3

--- js/script.js
+++ js/script.js
@@ -41,3 +41,60 @@ function deleteTodo(position){
 	todos.splice(position,1);
 	displayTodos();
 }
+//----------------------------
+//V3 What is an object?
+var myComputer = {
+	operatingSystem: 'mac',
+	screenSize: '15 inches',
+	purchaseYear: 2011
+};
+//access values by using dot notation w property names
+myComputer.operatingSystem //outputs 'mac'
+myComputer.screenSize//outputs '15 inches'
+
+var gordon = {
+	name: 'Gordon',
+	sayName: function(){
+		console.log(this.name);
+	}
+}
+//method= property that is a function on an object
+//V3 Requirements
+//It should store the todos array on an object
+//It should have a displayTodos method
+//It should have a an addTodo method
+//It should have a changeTodo method
+//It should have a deleteTodo method
+
+var todoList = {
+	todos: ['item 1', 'item 2', 'item 3'],
+	displayTodos: function (){
+		console.log('My todos: ', this.todos);
+	},
+	addTodos: function(todo){
+		this.todos.push(todo);
+		this.displayTodos();
+	},
+	changeTodo: function(position, newValue){
+		this.todos[position] = newValue;
+		this.displayTodos();
+	},
+	deleteTodo: function(position) {
+		this.todos.splice(position,1);
+		this.displayTodos();
+	}
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
