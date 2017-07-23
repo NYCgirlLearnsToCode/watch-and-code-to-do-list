6e2cf35e09f6cb102d15d240045b17b320c97676
LJ <imnotlostrui@aim.com>
2017-07-23 18:34:54 -0400

    added V4

--- js/script.js
+++ js/script.js
@@ -85,10 +85,37 @@ var todoList = {
 	}
 };
 
+//V4 Requirements
+// todoList.addTodo should add objects
+// todoList.changeTodo should change the todoText property
+// todoList.toggleCompleted should change the completed property
 
-
-
-
+var todoList = {
+	todos: [],
+	displayTodos: function (){
+		console.log('My todos: ', this.todos);
+	},
+	addTodos: function(todoText){
+		this.todos.push({
+			todoText: todoText;//2nd todoText is referring to func parameter
+			completed: false
+		});
+		this.displayTodos();
+	},
+	changeTodo: function(position, todoText){
+		this.todos[position].todoText = todoText;
+		this.displayTodos();
+	},
+	deleteTodo: function(position) {
+		this.todos.splice(position,1);
+		this.displayTodos();
+	},
+	toggleCompleted: function(position) {
+		var todo = this.todos[position];
+		todo.completed = !todo.completed;
+		this.displayTodos();
+	}
+};
 
 
 
