4f467e19dad6d0185dd0e34ee318897cce026aa4
LJ <imnotlostrui@aim.com>
2017-07-27 14:38:40 -0400

    added V8

--- js/script.js
+++ js/script.js
@@ -304,4 +304,100 @@ toggleAllButton.addEventListener('click', function() {
   todoList.toggleAll();
 });
 
+// V8 Requirements
+// It should have working controls for .addTodo
+// It should have working controls for .changeTodo
+// It should have working controls for .deleteTodo
+// It should have working controls for .toggleCompleted
+var todoList = {
+  todos: [],
+  displayTodos: function() {
+    if(this.todos.length === 0){
+      console.log('Your todo list is empty!');
+        } else {
+          console.log('My todos: ');
+          for(var i = 0; i < this.todos.length; i++) {
+              if(this.todos[i].completed === true){
+                console.log('(x)', this.todos[i].todoText);
+              }else{
+                console.log('( )' , this.todos[i].todoText)
+              }
+          }
+        }
+  },  
+  addTodo: function(todoText){
+    this.todos.push({
+      todoText: todoText,//2nd todoText is referring to parameter
+      completed:false
+    });
+    this.displayTodos();
+  },
+  changeTodo: function(position, todoText){
+    this.todos[position].todoText = todoText;
+    this.displayTodos();
+  },
+  deleteTodo: function(position){
+  this.todos.splice(position,1);
+    this.displayTodos();
+  },
+  toggleCompleted: function(position) {
+    var todo = this.todos[position];
+    todo.completed = !todo.completed;
+    this.displayTodos();
+  
+  },
+  toggleAll: function() {
+    var totalTodos = this.todos.length;
+    var completedTodos = 0;
+    //Get number of completed todos
+    for(var i = 0; i<totalTodos; i++){
+      if(this.todos[i].completed === true){
+        completedTodos++;
+      }
+    }
+    //If everything's true, make it false
+    if(completedTodos === totalTodos) {
+      for(var i = 0; i<totalTodos; i++){
+        this.todos[i].completed = false;
+      }
+      // .toggleAll: Otherwise, make everything true
+    }else{
+      for(var i = 0; i<totalTodos; i++){
+        this.todos[i].completed = true;
+      }
+    }
+    
+    this.displayTodos();
+  }
+};
 
+
+var handlers = {
+  displayTodos: function() {
+    todoList.displayTodos();
+  },
+  addTodo : function() {
+    var addTodoTextInput = document.getElementById('addTodoTextInput');
+    todoList.addTodo(addTodoTextInput.value);
+    addTodoTextInput.value = '';// clears input box after clicking add todo
+  },
+  changeTodo: function() {
+    var changeTodoPositionInput = document.getElementById('changeTodoPositionInput');
+    var changeTodoTextInput = document.getElementById('changeTodoTextInput');
+    todoList.changeTodo(changeTodoPositionInput.valueAsNumber, changeTodoTextInput.value);
+    changeTodoPositionInput.value='';
+    changeTodoTextInput.value='';
+  },
+  deleteTodo: function() {
+    var deleteTodoPositionInput = document.getElementById('deleteTodoPositionInput');
+    todoList.deleteTodo(deleteTodoPositionInput.valueAsNumber);
+    deleteTodoPositionInput.value= '';
+  },
+  toggleTodo: function() {
+    var toggleCompletedPositionInput = document.getElementById('toggleCompletedPositionInput');
+    todoList.toggleCompleted(toggleCompletedPositionInput.valueAsNumber);
+  },
+  toggleAll: function() {
+    todoList.toggleAll();
+  }
+};
